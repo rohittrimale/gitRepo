3607bf5a1d69bf238b70971d742103ee5ef4afb7
rohittrimale <rohittrimalle@gmail.com>
2024-03-04 22:21:48 +0530

    GetElementByID code

---
+++ getElementById/index.js
@@ -0,0 +1,15 @@
+const header = document.getElementById("header");
+const mainHeading = document.getElementById("main-heading");
+
+header.style.backgroundColor = "green";
+header.style.borderBottom = "3px solid orange";
+
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "orange";
+const basketHeading = document.getElementById("basket-heading");
+
+basketHeading.style.color = "green";
+
+const thanks = document.getElementById("thanks");
+
+thanks.innerHTML = "<p>Please visit us again</p>";
