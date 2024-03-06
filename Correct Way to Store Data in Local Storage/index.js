56513dcfebff228c30b7fd153e38012e9e4cb35b
rohittrimale <rohittrimalle@gmail.com>
2024-03-06 20:00:14 +0530

    Coorecct way to store data in local storage

---
+++ Correct Way to Store Data in Local Storage/index.js
@@ -0,0 +1,16 @@
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const name = event.target.username.value;
+  const email = event.target.email.value;
+  const phone = event.target.phone.value;
+
+  const details = {
+    username: name,
+    email: email,
+    phone: phone,
+  };
+
+  const detail = JSON.stringify(details);
+  localStorage.setItem("User Details", detail);
+}
